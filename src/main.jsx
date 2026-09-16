1b7d675813759a9a7a99f3c5038a7c210e8b0172
Salva <sgorlatt@gmail.com>
2026-09-16 14:48:01 +0200

    feat: add Electron support and configure build settings
    - Updated package.json to include Electron and electron-builder dependencies.
    - Added Electron entry point and build configuration for Windows.
    - Introduced new scripts for running and building the Electron app.
    - Added favicon.ico to the public directory.
    - Switched from BrowserRouter to HashRouter in main.jsx for better compatibility with Electron.
    - Updated vite.config.js to set the base path for assets.

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,8 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
-import { BrowserRouter } from "react-router-dom";
+// import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import { AuthProvider } from "./context/AuthContext.jsx";
 import { NotificationProvider } from "./context/NotificationContext.jsx";
 import "./index.css";
@@ -9,12 +10,12 @@ import "./App.css";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <AuthProvider>
         <NotificationProvider>
           <App />
         </NotificationProvider>
       </AuthProvider>
-    </BrowserRouter>
-  </StrictMode>
+    </HashRouter>
+  </StrictMode>,
 );
